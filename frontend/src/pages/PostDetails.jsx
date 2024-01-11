c5ebe56ea04beaf9264f5cf5869904d46ec6d475
hanzalaanees281 <hanzalaanees555@gmail.com>
2024-01-11 13:02:54 +0500

    Add comments and loader components and initial frontend complete

--- frontend/src/pages/PostDetails.jsx
+++ frontend/src/pages/PostDetails.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import { BiEdit } from "react-icons/bi"
 import { MdDelete } from "react-icons/md"
+import Comments from '../components/Comments'
 
 const PostDetails = () => {
     return (
@@ -42,35 +43,9 @@ const PostDetails = () => {
                 <div className="flex flex-col mt-4">
                     <h3 className="mt-6 mb-4 font-semibold">Comments: </h3>
                     {/*comment*/}
-                    <div className='px-2 py-2 bg-gray-200 rounded-xl my-2'>
-                        <div className='flex items-center justify-between'>
-                            <h3 className='font-bold text-gray-600'>@hanzalaanees</h3>
-                            <div className='flex justify-center items-center space-x-4'>
-                                <p className='text-gray-500 text-sm'>10/1/2024</p>
-                                <p className='text-gray-500 text-sm'>7:23</p>
-                                <div className="flex items-center justify-center space-x-2">
-                                    <p><BiEdit /></p>
-                                    <p><MdDelete /></p>
-                                </div>
-                            </div>
-                        </div>
-                        <p className='px-4 mt-2'>Nice information!!</p>
-                    </div>
-                    {/*comment*/}
-                    <div className='px-2 py-2 bg-gray-200 rounded-xl my-2'>
-                        <div className='flex items-center justify-between'>
-                            <h3 className='font-bold text-gray-600'>@hanzalaanees</h3>
-                            <div className='flex justify-center items-center space-x-4'>
-                                <p className='text-gray-500 text-sm'>10/1/2024</p>
-                                <p className='text-gray-500 text-sm'>7:23</p>
-                                <div className="flex items-center justify-center space-x-2">
-                                    <p><BiEdit /></p>
-                                    <p><MdDelete /></p>
-                                </div>
-                            </div>
-                        </div>
-                        <p className='px-4 mt-2'>Nice information!!</p>
-                    </div>
+                    <Comments />
+                    <Comments />
+                  
                 </div>
                 {/* write a comment */}
                 <div className="w-full flex flex-col mt-4 md:flex-row">
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import Footer from "../components/Footer"
 
 const Signup = () => {
     return (
@@ -21,6 +22,7 @@ const Signup = () => {
                     </div>
                 </div>
             </div>
+            <Footer />
         </>
     )
 }
